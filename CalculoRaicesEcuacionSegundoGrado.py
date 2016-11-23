dde330c3dc41c08466601787a83f6a19f05c9812
David1396 <davichorocks1996@gmail.com>
2016-11-23 12:07:38 -0500

    Primera parte del calculo de raices

---
+++ CalculoRaicesEcuacionSegundoGrado.py
@@ -0,0 +1,7 @@
+## Ecuacion de segundo grado 
+print ('Modelo de la ecuacion  a*x^2 + b*x + c = 0')
+a = float(input('Valor de a: '))
+b = float(input('Valor de b: '))
+c = float(input('Valor de c: '))
+#calcular el discrminante
+disc = float(b**2-4*a*c)
