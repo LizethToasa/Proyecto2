42e7d0290630451cc6cfcdd3cacc6c264c429561
David1396 <davichorocks1996@gmail.com>
2016-11-23 13:20:55 -0500

    Ya esta listo

--- BuscarPalabra.py
+++ BuscarPalabra.py
@@ -1,16 +1,26 @@
 def creartxt():
-    archi=open('TEXTO.txt','w')
-    archi.close()
-    
-pal=str(input('Ingrese la palabara a buscar: ',pal)
+	archi=open('Texto.txt','w')
+	archi.close
 
-def leertxt():
-    archi=open('TEXTO.txt','r')
-    linea=archi.readline()
-        
-    while linea!="":
-        cadena=linea
-        cadena.find(pal)
-        
-        linea=archi.readline()
+def grabartxt():
+        archi=open('TEXTO.txt','a')
+        archi.write('Poquito a poco te llevo al cielo amor poquito a poco yo te inundare de amor poquito a poco te llevo al cielo amor oh yo te inundare de amor')
         archi.close()
+
+creartxt()
+grabartxt()
+
+palabra = raw_input("palabra a buscar ?? ")
+
+repetidas = 0 
+f = open('TEXTO.txt','r')
+lines = f.readlines()
+for line in lines:
+    palabras = line.split(' ')
+    for p in palabras:
+        if p==palabra:
+            repetidas = repetidas+1
+
+print "la palabra "+str(palabra)+" se repite "+str(repetidas)+" veces"
+
+f.close()
