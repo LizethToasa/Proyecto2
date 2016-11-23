7c032439fc30e7dab900a13eb838168305bdfef2
LizethToasa <lizeth.toasa@gmail.com>
2016-11-23 12:12:43 -0500

    la siguiente parte

--- CalculoRaicesEcuacionSegundoGrado.py
+++ CalculoRaicesEcuacionSegundoGrado.py
@@ -5,3 +5,8 @@ b = float(input('Valor de b: '))
 c = float(input('Valor de c: '))
 #calcular el discrminante
 disc = float(b**2-4*a*c)
+if disc < 0:
+    print('No hay solucion')
+elif disc ==0:
+    soluc == float(-b/2*a)
+    print ('Solucion unica: ',soluc)
